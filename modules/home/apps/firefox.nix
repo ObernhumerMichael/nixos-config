07d456fc92e7823ebfde2a2de1b4f1d589d9ecd3
Michael Obernhumer <michaelobernhumer@gmail.com>
2025-08-16 17:25:46 +0200

    feat(apps): added plugins to firefox

--- modules/home/apps/firefox.nix
+++ modules/home/apps/firefox.nix
@@ -8,6 +8,13 @@
       id = 0;
       name = "default";
       isDefault = true;
+
+      search.default = "ddg";
+      search.order = [
+        "ddg"
+        "google"
+      ];
+
       # needed for Stylix to override extensions cleanly
       extensions.force = true;
     };
@@ -19,6 +26,20 @@
 
     policies = {
       DisplayBookmarksToolbar = "always";
+      ExtensionSettings = {
+        "uBlock0@raymondhill.net" = {
+          default_area = "menupanel";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+          private_browsing = true;
+        };
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          default_area = "menupanel";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+          private_browsing = true;
+        };
+      };
     };
   };
 }
