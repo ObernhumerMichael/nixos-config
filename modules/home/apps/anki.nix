fb1f446af616babc87523fec1199a1b2504e82a3
Michael Obernhumer <michaelobernhumer@gmail.com>
2025-09-30 18:24:44 +0200

    feat(apps): added anki-connect plugin to anki

--- modules/home/apps/anki.nix
+++ modules/home/apps/anki.nix
@@ -3,5 +3,21 @@
 {
   programs.anki = {
     enable = true;
+    addons = [
+      (pkgs.ankiAddons.anki-connect.withConfig {
+        config = {
+          apiKey = null;
+          apiLogPath = null;
+          webBindAddress = "127.0.0.1";
+          webBindPort = 8765;
+          webCorsOrigin = "http://localhost";
+          webCorsOriginList = [
+            "http://localhost"
+            "app://obsidian.md"
+          ];
+        };
+      })
+    ];
+
   };
 }
