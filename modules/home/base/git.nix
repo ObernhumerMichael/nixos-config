c90b418ead673b9e7f147d0ed84782b0e43afb48
Michael Obernhumer <michaelobernhumer@gmail.com>
2025-12-10 21:43:15 +0100

    feat(apps): added sigining of commit to git

--- modules/home/base/git.nix
+++ modules/home/base/git.nix
@@ -9,9 +9,20 @@
         name = "Michael Obernhumer";
         email = "michaelobernhumer@gmail.com";
       };
+      commit = {
+        gpgSign = true;
+      };
       color.ui = "auto";
       init.defaultBranch = "main";
       pull.rebase = false;
+    };
+
+    # GPG signing configuration
+    signing = {
+      format = "openpgp"; # use OpenPGP (your YubiKey)
+      key = "0x335020D3DDAB1C93"; # the signing subkey on YubiKey
+      signByDefault = true; # sign all commits and tags
+      # signer = null   # optional: path to gpg if needed, usually auto-detected
     };
   };
 
