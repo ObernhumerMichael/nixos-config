cbf2fcfaec46390d9ff5c3e84e18d5e35c13a86e
Michael Obernhumer <michaelobernhumer@gmail.com>
2025-12-29 21:30:43 +0100

    feat(security): added a firewall

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -10,5 +10,12 @@
       enable = true;
       plugins = [ pkgs.networkmanager-openconnect ];
     };
+
+    firewall = {
+      enable = true;
+      allowPing = true;
+      allowedTCPPorts = [ 22 ];  # SSH
+      allowedUDPPorts = [ 5353 ];  # mDNS for local discovery
+    };
   };
 }
