3d93550d8f04512a772d255a35fffa97860a8645
Michael Obernhumer <michaelobernhumer@gmail.com>
2025-12-29 20:21:30 +0100

    feat(shell): made some improvements

--- modules/home/base/shell.nix
+++ modules/home/base/shell.nix
@@ -3,17 +3,40 @@
 {
   programs.zsh = {
     enable = true;
-    autosuggestion.enable = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
+
+    autosuggestion = {
+      enable = true;
+      strategy = [
+        "history"
+        "completion"
+      ];
+    };
+
+    history = {
+      path = "$HOME/.zsh_history";
+      size = 100000;
+      save = 100000;
+      ignoreDups = true;
+      ignoreAllDups = true;
+      share = true;
+      extended = true;
+    };
 
     oh-my-zsh = {
       enable = true;
       plugins = [
         "git"
+        "sudo"
+        "extract"
+        "colored-man-pages"
       ];
     };
   };
 
-  programs.starship.enable = true;
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+  };
 }
