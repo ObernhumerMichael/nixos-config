75f8f4a5db777c1ed200185e5a2125c7dc3287ff
Michael Obernhumer <michaelobernhumer@gmail.com>
2026-07-20 18:24:26 +0200

    fix(gnome): removed the systemd service that autoreloads blur my shell

--- modules/home/gnome/extensions/blur-my-shell.nix
+++ modules/home/gnome/extensions/blur-my-shell.nix
@@ -1,13 +1,5 @@
 { config, pkgs, ... }:
 
-let
-  toggleBlurMyShell = pkgs.writeShellScript "toggle-blur-my-shell" ''
-    sleep 5
-    ${pkgs.gnome-shell}/bin/gnome-extensions disable blur-my-shell@aunetx
-    sleep 1
-    ${pkgs.gnome-shell}/bin/gnome-extensions enable blur-my-shell@aunetx
-  '';
-in
 {
   home.packages = with pkgs.gnomeExtensions; [
     blur-my-shell
@@ -25,21 +17,6 @@ in
 
     "org/gnome/shell/extensions/blur-my-shell/dash-to-dock" = {
       static-blur = false;
-    };
-  };
-
-  systemd.user.services.blur-my-shell-fix = {
-    Unit = {
-      Description = "Restart Blur My Shell to fix top panel blur glitch on login";
-      After = [ "graphical-session.target" ];
-      PartOf = [ "graphical-session.target" ];
-    };
-    Service = {
-      Type = "oneshot";
-      ExecStart = "${toggleBlurMyShell}";
-    };
-    Install = {
-      WantedBy = [ "graphical-session.target" ];
     };
   };
 }
